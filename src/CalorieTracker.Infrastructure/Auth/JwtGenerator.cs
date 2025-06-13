19f2f2e155312459df54eaec7135bc669dede356
Dominik Porębski <dp305922@student.polsl.pl>
2025-06-13 22:51:25 +0200

    Change JWT lifecycle duration

--- src/CalorieTracker.Infrastructure/Auth/JwtGenerator.cs
+++ src/CalorieTracker.Infrastructure/Auth/JwtGenerator.cs
@@ -28,7 +28,7 @@ namespace CalorieTracker.Infrastructure.Auth
 				issuer: _config["Jwt:Issuer"],
 				audience: _config["Jwt:Audience"],
 				claims: claims,
-				expires: DateTime.UtcNow.AddMinutes(1),
+				expires: DateTime.UtcNow.AddHours(3),
 				signingCredentials: creds
 			);
 
